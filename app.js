913860ef807e02b9b27fbaa641aa37cde3789b0c
oscq12 <orianasofia86@gmail.com>
2022-07-13 14:01:56 +0000

    Domain generator

---
+++ app.js
@@ -0,0 +1,16 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big', 'best','cool'];
+let noun = ['jogger','racoon','web','chat'];
+let direccion = ['.com','.es','.io','.es','.net'];
+
+
+let pronoun_add = Math.floor(Math.random()*pronoun.length);
+let adj_add = Math.floor(Math.random()*adj.length);
+let noun_add = Math.floor(Math.random()*noun.length);
+let direccion_add = Math.floor(Math.random()*direccion.length)
+
+
+
+
+console.log(pronoun[pronoun_add]+adj[adj_add]+noun[noun_add]+direccion[direccion_add])
+
