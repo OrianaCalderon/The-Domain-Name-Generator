1ddc453c76760a6b5e2c247c31d7e1a6e324fba0
oscq12 <orianasofia86@gmail.com>
2022-07-13 14:04:26 +0000

    Correccioón

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 let pronoun = ['the','our'];
 let adj = ['great', 'big', 'best','cool'];
-let noun = ['jogger','racoon','web','chat'];
-let direccion = ['.com','.es','.io','.es','.net'];
-
+let noun = ['jogger','racoon','web','chat','weebsite'];
+let direccion = ['.com','.es','.io','.es','.net','.ve'];
 
 let pronoun_add = Math.floor(Math.random()*pronoun.length);
 let adj_add = Math.floor(Math.random()*adj.length);
 let noun_add = Math.floor(Math.random()*noun.length);
 let direccion_add = Math.floor(Math.random()*direccion.length)
-
-
-
 
 console.log(pronoun[pronoun_add]+adj[adj_add]+noun[noun_add]+direccion[direccion_add])
 
